1e860e6fc10b24ccdd229e498f3795c5088306f6
max <max.vissions@gmail.com>
2025-11-28 16:26:03 +0600

    fix errors

--- src/Infrastructure/Soap/XRoadHeaderFactory.php
+++ src/Infrastructure/Soap/XRoadHeaderFactory.php
@@ -5,10 +5,13 @@ namespace App\Infrastructure\Soap;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Ramsey\Uuid\Uuid;
 use SoapHeader;
+use SoapVar;
+use stdClass;
 
 class XRoadHeaderFactory
 {
     private const NS = 'http://x-road.eu/xsd/xroad.xsd';
+    private const ID_NS = 'http://x-road.eu/xsd/identifiers';
 
     public function __construct(
         #[Autowire('%env(TUNDUK_CLIENT_CLASS)%')]
@@ -27,31 +30,45 @@ class XRoadHeaderFactory
         string $memberCode,
         string $instance = 'central-server',
         string $memberClass = 'GOV',
-        string $serviceVersion = 'v1'): array
-    {
+        string $serviceVersion = 'v1'
+    ): array {
+        $clientData = new stdClass();
+        $clientData->xRoadInstance = $instance;
+        $clientData->memberClass   = $this->memberClass;
+        $clientData->memberCode    = $this->memberCode;
+        $clientData->subsystemCode = $this->subSystemCode;
+        $clientData->objectType    = 'SUBSYSTEM';
+
+        $clientVar = new SoapVar(
+            $clientData,
+            SOAP_ENC_OBJECT,
+            'XRoadClientIdentifierType',
+            self::ID_NS
+        );
+
+        $serviceData = new stdClass();
+        $serviceData->xRoadInstance = $instance;
+        $serviceData->memberClass   = $memberClass;
+        $serviceData->memberCode    = $memberCode;
+        $serviceData->subsystemCode = $serviceSybSystem;
+        $serviceData->serviceCode   = $serviceCode;
+        $serviceData->serviceVersion = $serviceVersion;
+        $serviceData->objectType    = 'SERVICE';
+
+        $serviceVar = new SoapVar(
+            $serviceData,
+            SOAP_ENC_OBJECT,
+            'XRoadServiceIdentifierType',
+            self::ID_NS
+        );
+
         return [
-            new SoapHeader(self::NS, 'client', [
-                'objectType' => 'SUBSYSTEM',
-                'xRoadInstance' => $instance,
-                'memberClass' => $this->memberClass,
-                'memberCode' => $this->memberCode,
-                'subsystemCode' => $this->subSystemCode,
-            ]),
-
-            new SoapHeader(self::NS, 'service', [
-                'objectType' => 'SERVICE',
-                'xRoadInstance' => $instance,
-                'memberClass' => $memberClass,
-                'memberCode' => $memberCode,
-                'subsystemCode' => $serviceSybSystem,
-                'serviceCode' => $serviceCode,
-                'serviceVersion' => $serviceVersion,
-            ]),
-
+            new SoapHeader(self::NS, 'client', $clientVar),
+            new SoapHeader(self::NS, 'service', $serviceVar),
             new SoapHeader(self::NS, 'id', Uuid::uuid4()->toString(), true),
             new SoapHeader(self::NS, 'userId', 'DFA', true),
-            new SoapHeader(self::NS, 'protocolVersion', '4.0', true),
             new SoapHeader(self::NS, 'issue', 'GatewayRequest', true),
+            new SoapHeader(self::NS, 'protocolVersion', '4.0', true),
         ];
     }
 }
